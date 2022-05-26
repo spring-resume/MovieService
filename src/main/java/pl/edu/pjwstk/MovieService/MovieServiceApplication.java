fd2b0b44980c9307ca0d585feeb702e2fb54630f
Kuba <s18552@pjwstk.edu.pl>
2022-05-26 17:39:17 +0200

    no changes

--- src/main/java/pl/edu/pjwstk/MovieService/MovieServiceApplication.java
+++ src/main/java/pl/edu/pjwstk/MovieService/MovieServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MovieServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MovieServiceApplication.class, args);
+    }
 
 }
