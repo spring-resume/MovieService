33b6d01813dcfb8ff9fb02c8c1cf54f6c8ff9f88
Kuba <s18552@pjwstk.edu.pl>
2022-05-26 17:40:10 +0200

    add new ExceptionHandlers for new exceptions

--- src/main/java/pl/edu/pjwstk/MovieService/controller/GlobalHandlerException.java
+++ src/main/java/pl/edu/pjwstk/MovieService/controller/GlobalHandlerException.java
@@ -1,11 +1,10 @@
 package pl.edu.pjwstk.MovieService.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import pl.edu.pjwstk.MovieService.exception.NotChangedAvailabilityException;
 import pl.edu.pjwstk.MovieService.exception.NotFoundMovieException;
 
 @RestControllerAdvice
@@ -16,15 +15,18 @@ public class GlobalHandlerException {
         return ResponseEntity.status(404).body(ex.getMessage());
     }
 
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Received Invalid Input Parameters")
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<String> handleInvalidInputMovieException(HttpMessageNotReadableException ex) {
-        return ResponseEntity.status(400).body(ex.getMessage());
-
-//        Error error = new Error(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage());
-//        return new ResponseEntity<>(error, error.getHttpStatus()); // import z modelu error,
-//        responseentity z generykiem error,
-//        bez responsestatus
-//        https://www.amitph.com/spring-rest-api-custom-error-messages/
+    public ResponseEntity<String> handleInvalidInputMovieException() {
+        return ResponseEntity.status(400).body("Nieprawidłowe dane wejściowe");
     }
+
+    @ExceptionHandler(NotChangedAvailabilityException.class)
+    public ResponseEntity<String> handleInvalidChangeAvailabilityMovieException(NotChangedAvailabilityException ex) {
+        return ResponseEntity.status(406).body(ex.getMessage());
+    }
+
+//    @ExceptionHandler(NotRentException.class)
+//    public ResponseEntity<String> handleNotRentException(NotRentException ex) {
+//        return ResponseEntity.status(404).body(ex.getMessage());
+//    }
 }
