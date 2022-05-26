e0642e253890fddc842b517a5426cf6c2726d11a
Kuba <s18552@pjwstk.edu.pl>
2022-05-26 17:36:45 +0200

    add changeAvailable feature

--- src/main/java/pl/edu/pjwstk/MovieService/repository/MovieRepository.java
+++ src/main/java/pl/edu/pjwstk/MovieService/repository/MovieRepository.java
@@ -1,11 +1,13 @@
 package pl.edu.pjwstk.MovieService.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import pl.edu.pjwstk.MovieService.model.Movie;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
-
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
@@ -21,4 +23,9 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     @Override
     @SuppressWarnings("unchecked")
     Movie save(Movie entity);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Movie m SET m.isAvailable=:b WHERE m.id=:id")
+    void changeAvailable(boolean b, Long id);
 }
