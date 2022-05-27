a47195e773693309a26dad6cc341c0f2f3715ed2
Kuba <s18552@pjwstk.edu.pl>
2022-05-27 14:02:31 +0200

    add valid for rent and return movies

--- src/main/java/pl/edu/pjwstk/MovieService/service/MovieService.java
+++ src/main/java/pl/edu/pjwstk/MovieService/service/MovieService.java
@@ -3,6 +3,8 @@ package pl.edu.pjwstk.MovieService.service;
 import org.springframework.stereotype.Service;
 import pl.edu.pjwstk.MovieService.exception.NotChangedAvailabilityException;
 import pl.edu.pjwstk.MovieService.exception.NotFoundMovieException;
+import pl.edu.pjwstk.MovieService.exception.NotRentMovieException;
+import pl.edu.pjwstk.MovieService.exception.NotReturnMovieException;
 import pl.edu.pjwstk.MovieService.model.Movie;
 import pl.edu.pjwstk.MovieService.repository.MovieRepository;
 
@@ -46,27 +48,39 @@ public class MovieService {
         movieRepository.delete(takeMovie(id));
     }
 
+
     public Movie rentMovie(Long id) {
-        movieRepository.changeAvailable(false, id);
-        return checkAvailabilityForRent(id);
+        if (movieRepository.checkByAvailable(id))
+            movieRepository.changeAvailable(false, id);
+        else throw new NotRentMovieException(id);
+        return checkAvailabilityAfterRent(id);
     }
 
-    private Movie checkAvailabilityForRent(Long id) {
+//    public Movie rentMovie(Long id) {
+//        if(takeMovie(id).isAvailable())
+//         movieRepository.changeAvailable(false, id);
+//        else throw new NotRentException(id);
+//        return checkAvailabilityAfterRent(id);
+//}
+
+    private Movie checkAvailabilityAfterRent(Long id) {
         return Optional.of(takeMovie(id))
                 .filter(movie -> !movie.isAvailable())
                 .orElseThrow(NotChangedAvailabilityException::new);
     }
 
     public Movie returnMovie(Long id) {
-        movieRepository.changeAvailable(true, id);
-        return checkAvailabilityForReturn(id);
-
+        if (!(movieRepository.checkByAvailable(id)))
+            movieRepository.changeAvailable(true, id);
+        else throw new NotReturnMovieException(id);
+        return checkAvailabilityAfterReturn(id);
 
     }
 
-    private Movie checkAvailabilityForReturn(Long id) {
+    private Movie checkAvailabilityAfterReturn(Long id) {
         return Optional.of(takeMovie(id))
                 .filter(Movie::isAvailable)
                 .orElseThrow(NotChangedAvailabilityException::new);
     }
+
 }
