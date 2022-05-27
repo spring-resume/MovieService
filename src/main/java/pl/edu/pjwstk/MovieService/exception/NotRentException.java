ccb963d0b9ece70c26aad27b433754e257977fd5
Kuba <s18552@pjwstk.edu.pl>
2022-05-27 14:01:37 +0200

    change NotRentMovieException

--- src/main/java/pl/edu/pjwstk/MovieService/exception/NotRentException.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.pjwstk.MovieService.exception;
-
-public class NotRentException extends RuntimeException {
-    public NotRentException(String message, Long id) {
-        super(message + " " + id);
-    }
-}
